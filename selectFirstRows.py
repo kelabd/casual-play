e1a4080a75abceb563e55bc7b4b44e4bda0ee816
kelabd <karam.elabd@gmail.com>
2023-10-26 01:57:36 -0700

    adding successful Leet submission

---
+++ selectFirstRows.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 01:55:26 2023
+
+@author: karam
+"""
+
+import pandas as pd
+from typing import List
+
+# sample data borrowed from previous problem with players and not employees
+data = [
+    {"player_id": 846, "name": "Mason", "age": 21, "position": "Forward", "team": "RealMadrid"},
+    {"player_id": 749, "name": "Riley", "age": 30, "position": "Winger", "team": "Barcelona"},
+    {"player_id": 155, "name": "Bob", "age": 28, "position": "Striker", "team": "ManchesterUnited"},
+    {"player_id": 583, "name": "Isabella", "age": 32, "position": "Goalkeeper", "team": "Liverpool"},
+    {"player_id": 388, "name": "Zachary", "age": 24, "position": "Midfielder", "team": "BayernMunich"},
+    {"player_id": 883, "name": "Ava", "age": 23, "position": "Defender", "team": "Chelsea"},
+    {"player_id": 355, "name": "Violet", "age": 18, "position": "Striker", "team": "Juventus"},
+    {"player_id": 247, "name": "Thomas", "age": 27, "position": "Striker", "team": "ParisSaint-Germain"},
+    {"player_id": 761, "name": "Jack", "age": 33, "position": "Midfielder", "team": "ManchesterCity"},
+    {"player_id": 642, "name": "Charlie", "age": 36, "position": "Center-back", "team": "Arsenal"},
+]
+
+df = pd.DataFrame(data)
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    
+    return employees.head(3)
+
+selectFirstRows(df)
